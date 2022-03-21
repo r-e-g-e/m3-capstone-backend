1388e0b066f486463ed80a97f7eb5df877c2f27d
Rodrigo Tavares <rodrigotvrs70@hotmail.com>
2022-03-21 19:50:53 -0300

    added cors

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express from "express"
 import "express-async-errors"
 import { routes } from "./routes"
 import { handleAsyncErrors } from "./middlewares/asyncErrorsMiddleware"
+import cors from "cors"
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use(handleAsyncErrors)
